5c665f8cad6cd8f30aecff05e810bf5f28670a4e
veljko-d <veljko.d@protonmail.com>
2019-06-04 17:34:34 +0200

    Fix UpdateFolderRequest

--- app/Http/Requests/Folder/UpdateFolderRequest.php
+++ app/Http/Requests/Folder/UpdateFolderRequest.php
@@ -8,12 +8,14 @@ use Illuminate\Support\Facades\Auth;
 /**
  * Class UpdateFolderRequest
  *
- * @package App\Http\Requests\Folder
+ * @package App\Http\Requests
  */
 class UpdateFolderRequest extends FormRequest
 {
     /**
-     * @return int|null
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
     public function authorize()
     {
@@ -21,12 +23,16 @@ class UpdateFolderRequest extends FormRequest
     }
 
     /**
+     * Get the validation rules that apply to the request.
+     *
      * @return array
      */
     public function rules()
     {
         return [
+            'id'          => 'required|exists:folders,id',
             'folder_name' => 'required|min:1|max:50',
+            'parent_id'   => 'nullable|exists:folders,id',
         ];
     }
 }
