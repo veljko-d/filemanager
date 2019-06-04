2ed43f6720323add0a4ca8ff19e9cf9e1f53319d
veljko-d <veljko.d@protonmail.com>
2019-06-04 14:01:57 +0200

    Add Folder repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Folder\FolderRepository;
+use App\Repositories\Folder\FolderRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            FolderRepositoryInterface::class,
+            FolderRepository::class
+        );
     }
 
     /**
---
+++ app/Repositories/Folder/FolderRepository.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Repositories\Folder;
+
+use App\Models\Folder;
+
+/**
+ * Class FolderRepository
+ *
+ * @package App\Repositories\User
+ */
+class FolderRepository implements FolderRepositoryInterface
+{
+    /**
+     * @return mixed
+     */
+    public function index()
+    {
+        return Folder::noParent()->paginate(50);
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return mixed|void
+     */
+    public function store(array $data)
+    {
+        Folder::create($data);
+    }
+
+    /**
+     * @param $id
+     *
+     * @return Folder|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|mixed
+     */
+    public function show($id)
+    {
+        return Folder::with(['parent', 'children', 'files'])
+            ->where('folders.id', $id)
+            ->firstOrFail();
+    }
+
+    /**
+     * @param array $data
+     * @param       $id
+     *
+     * @return mixed|void
+     */
+    public function update(array $data, $id)
+    {
+        //Folder::where('id', $id)->update($data);
+    }
+
+    /**
+     * @param $id
+     *
+     * @return mixed|void
+     */
+    public function destroy($id)
+    {
+        Folder::destroy($id);
+    }
+}
